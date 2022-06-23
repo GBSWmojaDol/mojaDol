81bab5a47c9b3124d77abc08dfaa7110062510c2
MINGYU <90837142+mingyu9570@users.noreply.github.com>
2022-06-23 11:00:33 +0900

    Update checkInfo.js

--- CapStone/Capstone/js/checkInfo.js
+++ CapStone/Capstone/js/checkInfo.js
@@ -3,17 +3,37 @@ function CheckInfo() {
     let pw = document.querySelector('.pw');
     let pw_chk = document.querySelector('.pw-chk');
     let addr = document.querySelector('.addr');
+    let nick = document.querySelector('.nickname');
 
-    if (id.value === "" && pw.value === "" && pw_chk.value === "" && addr.value === "") {
-        alert("공백이 있습니다 확인을 해주세요");
+    if (id.value == "") {
+        alert("아이디를 입력해주세요");
         return false;
     }
-    else {
-        if (pw.value !== pw_chk.value) {
-            alert("비밀번호와 비밀번호 확인이 다릅니다. 다시 한 번 확인해주세요.")
-            return false;
-        } else {
-            return true;
-        }
+
+    if (pw.value == "") {
+        alert("비밀번호를 입력해주세요");
+        return false;
     }
+
+    if (pw_chk.value == "") {
+        alert("비밀번호를 다시 한 번 입력해주세요");
+        return false;
+    }
+
+    if (pw.value != pw_chk.value) {
+        alert("일치하지 않습니다. 다시 한 번 확인해주세요");
+        return false;
+    }
+
+    if (nick.value == "") {
+        alert("닉네임을 입력해주세요");
+        return false;
+    }
+
+    if (addr.value == "") {
+        alert("주소를 입력해주세요");
+        return false;
+    }
+
+    return true;
 }
