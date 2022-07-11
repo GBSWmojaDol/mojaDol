29263a3197d0bbe843c78361070739c5be614688
Shell-by <63432079+Shell-by@users.noreply.github.com>
2022-07-11 11:40:18 +0900

    Update CreateAccount.blade.php

--- resources/views/CreateAccount.blade.php
+++ resources/views/CreateAccount.blade.php
@@ -46,7 +46,7 @@
 <script>
     function checkId() {
         let id = document.querySelector('#id').value;
-        let url = 'http://127.0.0.1:8000/api/login/' + id;
+        let url = {{env('url'));
 
         fetch(url, {
             headers: {
